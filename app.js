adcf1cb6831ac388b6f752abd935775eb5893c20
Snider <rob.snider@cardinalhealth.com>
2016-01-06 11:40:52 -0500

    bootstrapped

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
