295477a5be49c046c303e9fcd7af20abbc16c8bf
Snider <rob.snider@cardinalhealth.com>
2016-01-06 09:25:05 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+http.listen(port, () => {
+    console.log('listening on ' + port);
+})
